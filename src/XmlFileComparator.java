f219749473af258688564871a7d66ccd59e44ea8
Bouhlal Abdessamad <bouhlal.work@gmail.com>
2021-01-15 01:02:58 +0100

    testing visual studio git

--- src/XmlFileComparator.java
+++ src/XmlFileComparator.java
@@ -15,8 +15,6 @@ import java.util.Date;
 public class XmlFileComparator {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, ParseException, InterruptedException {
         File file = new File("C:\\Users\\Abdessamad\\Desktop\\Tr.txt");
-
-        if(file.delete())
     }
 
     private static void extracted() throws IOException, ParserConfigurationException, SAXException, ParseException, InterruptedException {
