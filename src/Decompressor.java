a25fe1e351a22c87d661e3207e7c8358ab45bdd9
Abdessamad <bouhlal.work@gmail.com>
2021-01-17 01:26:30 +0100

    Zipper and file merger

--- src/Decompressor.java
+++ src/Decompressor.java
@@ -1,20 +1,16 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.zip.DeflaterOutputStream;
-import java.util.zip.InflaterInputStream;
+import java.io.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 //Uncompressing a file using an InflaterInputStream
 class Decompressor {
     public static void main(String[] args) throws IOException {
-        unzip("C:\\Users\\bouhl\\OneDrive\\Bureau\\Road To\\CodingGamesAndChallenges\\JavaTraining\\TestFolder.zip");
+        unzip("C:\\Users\\bouhl\\OneDrive\\Bureau\\Road To\\CodingGamesAndChallenges\\JavaTraining\\TestFolder");
+        FileGrouper();
     }
 
     private static void unzip(String zipFilePath) {
-        File dir = new File(".");
+        File dir = new File("C:\\Users\\bouhl\\OneDrive\\Bureau\\Road To\\CodingGamesAndChallenges\\JavaTraining\\Uncompressed");
         // create output directory if it doesn't exist
         if (!dir.exists())
             dir.mkdirs();
@@ -27,7 +23,7 @@ class Decompressor {
             ZipEntry ze = zis.getNextEntry();
             while (ze != null) {
                 String fileName = ze.getName();
-                File newFile = new File("." + File.separator + fileName);
+                File newFile = new File(dir+ File.separator + fileName);
                 System.out.println("Unzipping to " + newFile.getAbsolutePath());
                 // create directories for sub directories in zip
                 new File(newFile.getParent()).mkdirs();
@@ -49,6 +45,42 @@ class Decompressor {
             e.printStackTrace();
         }
 
+    }
+
+    public static void FileGrouper() throws FileNotFoundException, IOException {
+        File dir = new File("C:\\Users\\bouhl\\OneDrive\\Bureau\\Road To\\CodingGamesAndChallenges\\JavaTraining\\Uncompressed");
+
+        // create obejct of PrintWriter for output file
+        PrintWriter pw = new PrintWriter("output.txt");
+
+        // Get list of all the files in form of String Array
+        String[] fileNames = dir.list();
+
+        // loop for reading the contents of all the files
+        // in the directory GeeksForGeeks
+        for (String fileName : fileNames) {
+            System.out.println("Reading from " + fileName);
+
+            // create instance of file from Name of
+            // the file stored in string Array
+            File f = new File(dir, fileName);
+
+            // create object of BufferedReader
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            //pw.println("Contents of file " + fileName);
+
+            // Read from current file
+            String line = br.readLine();
+            while (line != null) {
+
+                // write to the output file
+                pw.println(line);
+                line = br.readLine();
+            }
+            pw.flush();
+        }
+        System.out.println("Reading from all files" +
+                " in directory " + dir.getName() + " Completed");
     }
 
 
